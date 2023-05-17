6dbd51433797340355500f8bdbcfcf30133c342d
Arno Luyckx <110333535+BlocksOnChainCode@users.noreply.github.com>
2023-05-17 10:10:53 +0000

    Able to add users to db with form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
   def create
-    # Create the dummy harcode user
-    user = User.create(name: "John Doe", email: "tesstMail@gmail.com", password: "12345678")    
+    # Create a dummy user from the form data
+    @user = User.new(user_params)
+    
+    # Save the user
+    if @user.save
+      # If the user is saved successfully, redirect to the index action
+      redirect_to users_path
+    else
+      # If the user fails validation, render the index action again
+      render :index
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :first_name, :last_name)
   end
 end
